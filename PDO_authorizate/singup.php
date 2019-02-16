cf00b48c54d08772654c67638a3eaebcc2a3eb53
Vitaliy <gapon007@ukr.net>
2019-02-16 15:36:03 +0200

    Add sinup functional form

---
+++ PDO_authorizate/singup.php
@@ -0,0 +1,10 @@
+
+<form method="post" action="reg.php">
+	<input type="text" name="login" placeholder="Login">
+	<input type="password" name="password" placeholder="Password">
+<button type="submit" name="button">Registrate</button>
+</form>
+
+
+<a href="singnin.php">Authorizate page</a>
+
