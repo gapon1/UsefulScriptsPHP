c24c00ef66f7002ce905c0fe905eda8d6020abf7
Vitaliy <gapon007@ukr.net>
2019-02-16 18:09:07 +0200

    Last changes in master branche

--- PDO_authorizate/index.php
+++ PDO_authorizate/index.php
@@ -1,3 +1,22 @@
-<?php
-	
-	phpinfo();
+
+
+<h1>Main page</h1>
+
+<legend>Go to regisytrate
+<a href="singup.php">Registrate page</a>
+</legend>
+
+<hr/>
+
+
+<legend>Go to Log in
+<a href="singnin.php">Log in page</a>
+</legend>
+<hr/>
+
+
+
+<legend>Go to Admin page
+<a href="admin.php">Administrator page</a>
+</legend>
+<hr/>
