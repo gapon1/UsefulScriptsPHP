f2830946a68dc53ba749c060afeb7fd028fc37e6
Vitaliy <gapon007@ukr.net>
2019-02-16 15:58:38 +0200

    Add auth.php file AND realise Autontification functionaliti

---
+++ PDO_authorizate/auth.php
@@ -0,0 +1,39 @@
+
+<?php
+
+require_once "db.php";
+
+
+$login = trim($_POST['login']);
+$password = trim($_POST['password']);
+
+if (!empty($login) && !empty($password) ) {
+
+	
+$sql = 'SELECT login, password FROM users WHERE login = :login';
+
+
+$params = [':login' => $login];
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params);
+
+$user = $stmt->fetch(PDO::FETCH_OBJ);
+
+if ($user) {
+	if (password_verify($password, $user->password)) {
+		header('Location: admin.php');
+	}else{
+		echo "Error LOgin or Password 1";
+	}
+
+
+}else{
+	echo "Error LOgin or Password 2";
+}
+
+
+}else{
+	echo "Please fill all colums";
+}
+
