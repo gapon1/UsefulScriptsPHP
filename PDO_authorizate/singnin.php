a1fbf22bc246f2ec1348d3d32bca5d2ede33590e
Vitaliy <gapon007@ukr.net>
2019-02-16 15:50:16 +0200

    Add sing in file AND realise functional sing in

---
+++ PDO_authorizate/singnin.php
@@ -0,0 +1,12 @@
+
+
+<form method="post" action="auth.php">
+	<input type="text" name="login" placeholder="Login">
+	<input type="password" name="password" placeholder="Password">
+<button type="submit" name="button">Autorizate</button>
+</form>
+
+
+<a href="singup.php">Registrate page</a>
+
+
