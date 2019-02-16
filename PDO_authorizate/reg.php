1d064f2ccf09889bbc50ca250f947859f19edf25
Vitaliy <gapon007@ukr.net>
2019-02-16 21:12:45 +0200

    Check EXIST registration user login

--- PDO_authorizate/reg.php
+++ PDO_authorizate/reg.php
@@ -8,6 +8,25 @@ $password = trim($_POST['password']);
 
 if (!empty($login) && !empty($password) ) {
 
+
+	//==== Провірка нв існування користувача ======
+	$sql_check = 'SELECT EXISTS( SELECT login FROM users WHERE login = :login)';
+
+	$stmt_check = $pdo->prepare($sql_check);
+
+	$stmt_check->execute( [':login' => $login] );
+
+	if ($stmt_check->fetchColumn() ) {
+		die("User with choose login already EXIST !");
+	}
+
+
+
+
+
+
+	//===================================
+
 	$password = password_hash($password, PASSWORD_DEFAULT);
 	
 $sql = 'INSERT INTO users(login, password) VALUES(:login, :password)';
