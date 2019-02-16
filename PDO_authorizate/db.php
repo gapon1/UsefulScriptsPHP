05ad951d4f38ce1b01afb4dd2c2befd2e37b8f49
Vitaliy <gapon007@ukr.net>
2019-02-16 15:01:51 +0200

    Use NAmed plaseholders PDO

--- PDO_authorizate/db.php
+++ PDO_authorizate/db.php
@@ -20,17 +20,43 @@ $pdo = new PDO("$driver:host=$host;dbname=$db_name;charset=$charset", $db_user, 
 
 $result = $pdo->query('SELECT * FROM muvies2');
 
-// ======= Виводимо обрані нами значення з таблиці =======
+// ======= Виводимо обрані нами значення з таблиці ==========================
 // while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 // 	echo "Film "  .  $row['title'] .  " Duration "  . $row['duration']  .  " Minits  <br />";
 // }
 
 
-// ======== Метод fetchALL Який повертаэ всі строки таблиці в виді багатомірного масиву
+// ======== Метод fetchALL Який повертаэ всі строки таблиці в виді багатомірного масиву  ==============
 
-$rows = $result->fetchAll(PDO::FETCH_ASSOC);
+// $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+// echo "<pre>";
+// var_dump($rows);
+
+//========= Використання підготовлених запитів PLACEHOLDER ===================
+###### Іменовані плейсхолдери ##############
+
+$sql = 'SELECT title FROM muvies2 WHERE duration = :duration';
+
+$stmt = $pdo->prepare($sql);
+
+$params = [':duration' => '210'];
+
+$stmt->execute($params);
+
+$rows =$stmt->fetchAll(PDO::FETCH_ASSOC);
+
 echo "<pre>";
 var_dump($rows);
+
+
+###### Позиційні плейсхолдери ##############
+
+
+
+
+
+
+
 
 
 
