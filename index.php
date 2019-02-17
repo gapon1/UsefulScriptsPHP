781159845d9aa36d379d8d15f064429f7c25377b
Vitaliy <gapon007@ukr.net>
2019-02-17 14:29:35 +0200

    Reforma index file structure

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
 
 <h1>Main page</h1>
 
 <legend>Go to regisytrate
-<a href="singup.php">Registrate page</a>
+    <a href="singup.php">Registrate page</a>
 </legend>
 
 <hr/>
 
 
 <legend>Go to Log in
-<a href="singnin.php">Log in page</a>
+    <a href="singnin.php">Log in page</a>
 </legend>
 <hr/>
-
 
 
 <legend>Go to Admin page
-<a href="admin.php">Log in page</a>
+    <a href="admin.php">Log in page</a>
 </legend>
 <hr/>
+
+</body>
+</html>
+
